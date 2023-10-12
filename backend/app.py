2e938de47d23035844d015289508a029b080f456
Amelia Paysinger <ameliapaysinger@gmail.com>
2023-10-12 12:36:38 -0500

    removed comments, ran black

--- backend/app.py
+++ backend/app.py
@@ -5,6 +5,7 @@ from flask_cors import CORS
 from flask_migrate import Migrate
 from db import db
 from flask import Flask
+
 # from app import auth as venv_blueprint
 from main_bp import main as main_blueprint
 from auth import auth as auth_blueprint
@@ -12,43 +13,26 @@ from flask_login import LoginManager
 from models import User
 
 
-# load_dotenv()
-# app = Flask(__name__)
-# cors = CORS(app)
-# app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv("DATABASE_URL")
-
-# db = SQLAlchemy(app)
-# migrate = Migrate(app, db)
-
-
-# db = SQLAlchemy()
-
 def create_app():
     app = Flask(__name__)
     load_dotenv()
     cors = CORS(app)
-    app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv("DATABASE_URL")
+    app.config["SQLALCHEMY_DATABASE_URI"] = os.getenv("DATABASE_URL")
     app.secret_key = os.getenv("SECRET_KEY")
     db.init_app(app)
-    
 
     migrate = Migrate(app, db)
     migrate.init_app(app, db)
 
     login_manager = LoginManager()
-    login_manager.login_view = 'auth.login'
+    login_manager.login_view = "auth.login"
     login_manager.init_app(app)
 
     @login_manager.user_loader
     def load_user(user_id):
         return User.query.get(int(user_id))
 
-    # blueprint for auth routes in our app
-    # from .auth import auth as auth_blueprint
     app.register_blueprint(auth_blueprint)
-
-    # blueprint for non-auth parts of app
-    # from .main import main as main_blueprint
     app.register_blueprint(main_blueprint)
 
     return app
--- backend/migrations/versions/7d00ff3227e0_adding_users.py
+++ backend/migrations/versions/7d00ff3227e0_adding_users.py
@@ -12,21 +12,22 @@ import sqlalchemy as sa
 
 
 # revision identifiers, used by Alembic.
-revision: str = '7d00ff3227e0'
-down_revision: Union[str, None] = 'f9f75906daac'
+revision: str = "7d00ff3227e0"
+down_revision: Union[str, None] = "f9f75906daac"
 branch_labels: Union[str, Sequence[str], None] = None
 depends_on: Union[str, Sequence[str], None] = None
 
 
 def upgrade() -> None:
-    op.create_table('users',
-    sa.Column('id', sa.Integer(), nullable=False),
-    sa.Column('email', sa.String(), nullable=False),
-    sa.Column('password', sa.String(), nullable=False),
-    sa.Column('name', sa.String(), nullable=False),
-    sa.PrimaryKeyConstraint('id')
+    op.create_table(
+        "users",
+        sa.Column("id", sa.Integer(), nullable=False),
+        sa.Column("email", sa.String(), nullable=False),
+        sa.Column("password", sa.String(), nullable=False),
+        sa.Column("name", sa.String(), nullable=False),
+        sa.PrimaryKeyConstraint("id"),
     )
 
 
 def downgrade() -> None:
-    op.drop_table('users')
+    op.drop_table("users")
